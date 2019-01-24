8f1fb06d04f43cef3c95f4bbeafa750d9567ebf1
Sadegh Hmp <sghmp97@gmail.com>
2019-01-25 03:07:33 +0330

    add 'replaced var' for nodes

--- MyModule/parser.py
+++ MyModule/parser.py
@@ -24,9 +24,9 @@ class DepthParser(Parser):
 
     def parse(self, target):
         from MyModule.funcs import its_terminal, its_variable
-        start = Node(self.start_var, target)
+        start = Node(self.start_var, target, " ")
         for p in self.table[start.value]:
-            start.childs.append(Node(p, target))
+            start.childs.append(Node(p, target, start.value+"->"+p))
             if self.parse_inside(start.childs[-1]):
                 start.valid = True
                 return (start, True)
@@ -44,16 +44,20 @@ class DepthParser(Parser):
             if its_variable(c):
                 for p in self.table[c]:
                     new_value = node.value.replace(c, p, 1)
-                    node.childs.append(Node(new_value, node.target))
+                    node.childs.append(
+                        Node(new_value, node.target, c+"->" + p))
                     if self.parse_inside(node.childs[-1]):
                         node.valid = True
                         return True
+                    # else:
+                    #     node.childs.pop()
         return False
 
 
 class Node():
 
-    def __init__(self, value, target):
+    def __init__(self, value, target, p):
+        self.p = p
         self.valid = None
         self.value = value
         self.target = target
@@ -68,7 +72,7 @@ class Node():
                 break
             if self.value[i] != self.target[i]:
                 return False
-        for i in range(len(self.value)-1, -1, -1):
+        for i in range(-1,len(self.value)*-1 , -1):
             if its_variable(self.value[i]):
                 break
             if self.value[i] != self.target[i]:
